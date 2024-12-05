665533f74a8fbd4843dc8d40fe07905d419de3a4
Shad02w <alvistse123@gmail.com>
2024-12-04 20:18:27 -0500

    ad ncc

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as core from "@actions/core";
 async function run() {
 	try {
 		const patterns = core.getInput("patterns");
+		console.log(patterns);
 	} catch (e) {
 		if (e instanceof Error) {
 			core.setFailed(e.message);
